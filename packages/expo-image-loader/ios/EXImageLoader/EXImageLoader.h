73fdb042481276227f4b00079771e3a74f536baf
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-11-28 15:59:06 +0100

    Move loadImageForUrl to EXImageLoader

---
+++ packages/expo-image-loader/ios/EXImageLoader/EXImageLoader.h
@@ -0,0 +1,8 @@
+#import <UMCore/UMInternalModule.h>
+#import <UMImageLoaderInterface/UMImageLoaderInterface.h>
+#import <UMReactNativeAdapter/UMBridgeModule.h>
+#import <UIKit/UIKit.h>
+
+@interface EXImageLoader : NSObject<UMBridgeModule, UMInternalModule, UMImageLoaderInterface>
+
+@end
