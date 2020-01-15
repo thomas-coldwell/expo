6825f578359db8cf2a6baf0c9421488a370074ba
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-15 14:55:57 +0100

    [expo-notifications] Implement listening to messages on Android
    FirebaseListenerService handles new message events, informs NotificationManagers which in turn call appropriate methods on NotificationListeners (NotificationsModules). NotificationsModules emit events to JS.

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -6,20 +6,28 @@ import org.unimodules.core.BasePackage;
 import org.unimodules.core.ExportedModule;
 import org.unimodules.core.interfaces.SingletonModule;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
+import expo.modules.notifications.notifications.NotificationManager;
+import expo.modules.notifications.notifications.NotificationsEmitter;
 import expo.modules.notifications.tokens.PushTokenManager;
 import expo.modules.notifications.tokens.PushTokenModule;
 
 public class NotificationsPackage extends BasePackage {
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
-    return Collections.singletonList((ExportedModule) new PushTokenModule(context));
+    return Arrays.asList(
+        new PushTokenModule(context),
+        new NotificationsEmitter(context)
+    );
   }
 
   @Override
   public List<SingletonModule> createSingletonModules(Context context) {
-    return Collections.singletonList((SingletonModule) new PushTokenManager());
+    return Arrays.asList(
+        new PushTokenManager(),
+        new NotificationManager()
+    );
   }
 }
